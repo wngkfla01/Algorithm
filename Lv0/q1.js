b0272b6f903cf8f0e2618cbaaddebfaaef7affa8
JuHarim <wngkfla01@gmail.com>
2023-05-15 16:28:50 +0900

    1번풀이완료

--- Lv0/q1.js
+++ Lv0/q1.js
@@ -13,8 +13,18 @@ rl.on("line", (line) => {
   // 한 줄씩 입력받은 후 실행할 코드. 입력값은 line에 저장
   console.log(line)
   rl.close() // 꼭 close 해줘야됨. 아니면 입력을 무한정 받음
+
+  // ✔️ on 메소드
+  // : 이벤트가 발생할 때 실행할 동작을 지정
+
+  // ✔️ line 이벤트
+  // : readline interface를 통해 다룰 이벤트. 사용자가 콘솔에 입력할 때 발생
+  //   입력 이벤트는 입력 스트림에 줄바꿈을 나타내는 \n, \r, or \r\n 제어 문자가 나타나거나, 사용자가 Enter 또는 Return을 누를 때 발생
 })
 rl.on('close', () => {
   // 입력이 끝난 후 실행할 코드
   process.exit()
+
+  // ✔️ close 이벤트
+  // : Readable 스트림 종료를 제어하는 이벤트
 })
